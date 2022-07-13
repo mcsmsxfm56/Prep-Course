d4cfdf15316b47b3fd11d9a164e406dccc63d49c
mcsmsxfm56 <mcuadrado135@gmail.com>
2022-07-13 10:32:28 -0300

    correcion jsII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -203,7 +203,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if(numero >= 99 && numero < 1000){
+  if(numero > 99 && numero < 1000){
     return true;
   } else {
     return false;
